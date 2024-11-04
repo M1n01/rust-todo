be82f127acdf5e6c96a58c69fcc5b5111468fbe6
M1n01 <abe.minato.bz@gmail.com>
2024-11-04 22:52:26 +0900

    5.5 update: Edit on TodoList

--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -1,7 +1,8 @@
-import { FC, ChangeEventHandler } from 'react';
-import { Card, Checkbox, Button, Group } from '@mantine/core';
-import { IconTrash } from '@tabler/icons-react';
+import { FC, ChangeEventHandler, useState, useEffect } from 'react';
+import { Card, Checkbox, Button, Group, Modal, TextInput, Stack, Box } from '@mantine/core';
+import { IconTrash, IconEdit } from '@tabler/icons-react';
 import type { Todo } from '../types/todo';
+import { modalInnerStyle } from '../styles/modal';
 
 type Props = {
   todo: Todo;
@@ -10,11 +11,27 @@ type Props = {
 };
 
 const TodoItem: FC<Props> = ({ todo, onUpdate, onDelete }) => {
-    const handleCompletedCheckbox: ChangeEventHandler = (e) => {
+  const [editing, setEditing] = useState(false);
+  const [editText, setEditText] = useState('');
+
+  // propsのtodo変更時に再度初期化
+  useEffect(() => {
+    setEditText(todo.text);
+  }, [todo]);
+
+  const handleCompletedCheckbox: ChangeEventHandler = (e) => {
     onUpdate({
       ...todo,
       completed: !todo.completed,
     });
+  };
+
+  const onCloseEditModal = () => {
+    onUpdate({
+      ...todo,
+      text: editText,
+    });
+    setEditing(false);
   };
 
   const handleDelete = () => onDelete(todo.id);
@@ -28,8 +45,27 @@ const TodoItem: FC<Props> = ({ todo, onUpdate, onDelete }) => {
           variant="outline"
           label={todo.text}
         />
-        <Button onClick={handleDelete} color="red" leftSection={<IconTrash />} variant="light">Delete</Button>
+        <Group>
+          <Button onClick={() => setEditing(true)} leftSection={<IconEdit />} variant="light">Edit</Button>
+          <Button onClick={handleDelete} color="red" leftSection={<IconTrash />} variant="light">Delete</Button>
+        </Group>
       </Group>
+      <Modal
+        opened={editing}
+        onClose={onCloseEditModal}
+        title="Edit Todo"
+      >
+        {/* <Box style={{
+          ...modalInnerStyle,
+          boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+        }}> */}
+        <TextInput
+          value={todo.text}
+          label="Todo text"
+          onChange={(e) => setEditText(e.target.value)}
+        />
+        {/* </Box> */}
+      </Modal>
     </Card>
   );
 };
